b3b96d6347164979f2b44446de6e40f9debd882c
Kevin Molina <kevinmolina.dev@gmail.com>
2023-04-16 21:03:34 -0400

    Agregando la logica

---
+++ js/app.js
@@ -0,0 +1,43 @@
+const texto = document.getElementById("entrada");
+const encriptar = document.getElementById("encriptar");
+const desencriptar = document.getElementById("desencriptar");
+const salida = document.getElementById("salida");
+const salidaOriginal = salida.innerHTML;
+
+texto.addEventListener("input",()=>{
+    texto.value = texto.value.replace(/[^a-zñ0-9\s]/g, "");
+})
+
+const encriptado = (texto) => {
+    return texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
+};
+
+const desencriptado = (texto) => {
+    return texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
+};
+
+encriptar.addEventListener("click", () => {
+    if (texto.value.length > 0) {
+        salida.innerHTML = `
+      <div class="flex-column output_text_after">
+        <p id="textResult" class="text">${encriptado(texto.value)}</p>
+      </div>
+      <button id="Copiar" class="output_btn" onclick="Copiar()">Copiar</button>
+    `;
+    } else {
+        salida.innerHTML = salidaOriginal;
+    }
+});
+
+desencriptar.addEventListener("click", () => {
+    if (texto.value.length > 0) {
+        salida.innerHTML = `
+      <div class="flex-column output_text_after">
+        <p id="textResult" class="text">${desencriptado(texto.value)}</p>
+      </div>
+      <button id="Copiar" class="output_btn" onclick="Copiar()">Copiar</button>
+    `;
+    } else {
+        salida.innerHTML = salidaOriginal;
+    }
+});
